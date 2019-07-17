b0378208f5b55eae433397492c11f67d164eb9d3
ion <i_1392010@qq.com>
2019-07-18 00:02:10 +0800

    second

---
+++ src/components/About.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="about container">
+    <h1 class="page-header">
+      <router-link to="/" class="btn btn-default">返回</router-link>
+    </h1>
+    <p class="jumbotron"><img src="static/bcg.jpg" alt=""></p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "about",
+  data() {
+    return {};
+  }
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+img{
+  width: 100%;
+}
+</style>
